91f46e60cdf5a3215a28cd2ce16f4351d072b922
Harshitha Ravindra <38550570+HarshithaRavindra29@users.noreply.github.com>
2019-11-10 15:48:01 -0600

    added input receive

--- fillomino.py
+++ fillomino.py
@@ -5,7 +5,7 @@ Authors: Gaurav Dharra, Harshitha Ravindra
 """
 
 import numpy as np
-
+import click
 
 def generate_grid(num_rows, num_columns):
     """
@@ -15,5 +15,27 @@ def generate_grid(num_rows, num_columns):
     :return: Returns a 2d array of the required size with all zeros
     """
     return np.zeros((num_rows, num_columns), int)
+
+
+
+def game_level():
+    """
+
+    :return:
+    """
+    game_option = click.prompt('Please select difficulty level: \n \
+        Enter 1 for easy (10 X 5) \n  \
+        Enter 2 for medium (10 X 10) \n \
+        Enter 3 for difficult (20 X 20)', type=click.IntRange(1, 3))
+    if game_option == 1:
+        m = 10
+        n = 5
+    elif game_option == 2:
+        m = 10
+        n = 10
+    else:
+        m = 20
+        n = 20
+    return m, n
 
 
