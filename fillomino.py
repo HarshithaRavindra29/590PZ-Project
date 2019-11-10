6a7c66a86bae5b21c4f598cf9c6e00b3d0b43499
gdharra2 <46692795+gdharra2@users.noreply.github.com>
2019-11-10 15:41:12 -0600

    Added generate grid method

--- fillomino.py
+++ fillomino.py
@@ -4,3 +4,16 @@ Fillomino Puzzle solver and Generator
 Authors: Gaurav Dharra, Harshitha Ravindra
 """
 
+import numpy as np
+
+
+def generate_grid(num_rows, num_columns):
+    """
+    Method that generates a 2d numpy array with all zero values for given number of row and column values.
+    :param num_rows: integer indicating number of rows
+    :param num_columns: integer indicating number of rows
+    :return: Returns a 2d array of the required size with all zeros
+    """
+    return np.zeros((num_rows, num_columns), int)
+
+
